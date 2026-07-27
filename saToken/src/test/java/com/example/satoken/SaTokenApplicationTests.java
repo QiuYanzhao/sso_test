acbb476957c761d4c890dc8483b2e529f1128f33
qiuyanzhao <qiuyanzhao@58.com>
2026-07-27 11:22:30 +0800

    init

---
+++ saToken/src/test/java/com/example/satoken/SaTokenApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.satoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
